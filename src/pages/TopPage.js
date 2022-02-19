b28d8f061324b9cd98c1094bfff463839fd799bf
ktakeuchi <takevadogu@outlook.jp>
2022-02-19 20:12:38 +0900

    Headerコンポーネントを追加

--- src/pages/TopPage.js
+++ src/pages/TopPage.js
@@ -1,3 +1,4 @@
+import Header from "../compornents/Header";
 import Title from "../compornents/Title";
 import Selector from "../compornents/Selector";
 import Results from "../compornents/Results";
@@ -7,6 +8,7 @@ const TopPage = (props)=>{
     return(
         <div className="top-page-container">
             <div>
+            <Header />
             <Title />
             <Selector countriesJson={props.countriesJson} setCountry={props.setCountry} 
             getCountryData={props.getCountryData} />
