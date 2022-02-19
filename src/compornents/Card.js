098656f079e33a4c303094589c19300f0b369937
ktakeuchi <takevadogu@outlook.jp>
2022-02-19 20:11:21 +0900

    .toLocaleString()を追加

--- src/compornents/Card.js
+++ src/compornents/Card.js
@@ -1,19 +1,17 @@
-
 const Card = (props)=> {
-
     return (
-        <div>
-            <h2>Card</h2>
-            <button onClick={props.getAllCountriesData}>Get All Data</button>
-            {props.allCountriesData.map((singleData, index)=>
-                <div key={index}>
-                    <h2>{singleData.Country}</h2>
-                    <p>新規感染者：{singleData.NewCofirmed}</p>
-                    <p>感染者総数：{singleData.TotalConfirmed}</p>
-                </div>
-            )}
+        <div className="card-container">
+            {props.allCountriesData.map((singleData, index) =>
+              <div key={index} className="card">
+                <div>
+                    <h2>{singleData.Country}</h2>                   
+                    <p>新規感染者: <span>{singleData.NewConfirmed.toLocaleString()}</span></p>      
+                    <p>感染者総数: <span>{singleData.TotalConfirmed.toLocaleString()}</span></p> 
+                </div>   
+              </div>
+            )} 
         </div>
     );
-    
 };
+            
 export default Card;
