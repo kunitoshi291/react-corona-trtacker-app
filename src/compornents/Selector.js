c2dacd04b483c95cc16abc9479e6e1a4f2e60442
ktakeuchi <takevadogu@outlook.jp>
2022-02-23 13:46:05 +0900

    optionタグbuttonタグを削除

--- src/compornents/Selector.js
+++ src/compornents/Selector.js
@@ -4,12 +4,10 @@ const Selector = ({setCountry, countriesJson, getCountryData})=> {
         <div className="selector-container">
             <select onChange={(e)=>
             setCountry(e.target.value)}>
-                <option>Select A Country</option>
                 {countriesJson.map((country,index)=>
                     <option key={index} value={country.Slug}>{country.Country}</option>
                 )};
             </select>
-            <button onClick={getCountryData}>Get data</button>
         </div>
     );
 };
